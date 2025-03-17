570f8d2b888e34e7d9c248693fdd58a25c9300bc
DerFacn <nikitalupalo145@gmail.com>
2025-03-17 12:36:58 +0100

    Hello world

---
+++ app/config.py
@@ -0,0 +1,13 @@
+from os import environ
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Config:
+    SECRET_KEY = environ.get('SECRET_KEY') or 'StandardKey'
+    CLIENT_ID = environ.get('CLIENT_ID')
+    CLIENT_SECRET = environ.get('CLIENT_SECRET')
+    AUTH_URL = environ.get('AUTH_URL')
+    TOKEN_URL = environ.get('TOKEN_URL')
+    USERINFO_URL = environ.get('USERINFO_URL')
