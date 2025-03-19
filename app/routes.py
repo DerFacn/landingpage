be8c5aedbd43c69348f93963a44c992c4be9f10f
DerFacn <nikitalupalo145@gmail.com>
2025-03-19 09:13:50 +0100

    Problems fix, now it's working

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
+from datetime import datetime, timedelta
 from flask import Flask, current_app, request, redirect, make_response
-from app.utils import get_access_token_with_code, refresh_access_token, get_userinfo
+from app.utils import get_tokens_with_code, refresh_access_token, get_userinfo
 
 
 def register_functions(app: Flask):
@@ -14,36 +15,42 @@ def index():
     if not access_token:
         client_id = current_app.config.get('CLIENT_ID')
         auth_url = current_app.config.get('AUTH_URL')
-        url = auth_url + f"?client_id={client_id}&response_type=code&redirect_uri=http://127.0.0.1:5000/auth&scope=openid%20profile"
+        redirect_uri = current_app.config.get('REDIRECT_URI')
+        url = auth_url + f"?client_id={client_id}&response_type=code&redirect_uri={redirect_uri}&scope=openid%20profile"
         return redirect(url)
     
     try:
         user_info = get_userinfo()
     except:
         access_token = refresh_access_token()
+        
+        try:
+            user_info = get_userinfo(access_token)
+        except:
+            resp = make_response(redirect('/'))
+            resp.set_cookie('refresh_token', '')
+            resp.set_cookie('access_token', '')
+            return resp
+ 
+    expiration = datetime.now() + timedelta(days=400)
+    resp = make_response({"message": user_info})
+    resp.set_cookie('access_token', access_token, expires=expiration)
 
-    try:
-        user_info = get_userinfo()
-    except:
-        response = make_response(redirect('/'))
-        response.set_cookie('refresh_token', '')
-        response.set_cookie('access_token', '')
-        return response
-
-    return {"message": user_info}
+    return resp
 
 
 def auth():
     code = request.args.get('code')
 
     try:
-        data = get_access_token_with_code(code)
+        data = get_tokens_with_code(code)
     except:
         return {"message": "Try again later"}
     
+    expiration = datetime.now() + timedelta(days=400)
     response = make_response(redirect('/'))
-    response.set_cookie('access_token', data.get('access_token'))
-    response.set_cookie('refresh_token', data.get('refresh_token'))
+    response.set_cookie('access_token', data.get('access_token'), expires=expiration)
+    response.set_cookie('refresh_token', data.get('refresh_token'), expires=expiration)
 
     return response
 
--- app/utils.py
+++ app/utils.py
@@ -1,11 +1,13 @@
+import base64
 import requests
 from flask import current_app, request
 
 
-def get_access_token_with_code(code: str):
+def get_tokens_with_code(code: str):
     token_url = current_app.config.get('TOKEN_URL')
     client_id = current_app.config.get('CLIENT_ID')
     client_secret = current_app.config.get('CLIENT_SECRET')
+    redirect_uri = current_app.config.get('REDIRECT_URI')
 
     if None in [token_url, client_secret, client_id]:
         raise Exception('Token url, client id or client secret is not provided!')
@@ -15,13 +17,16 @@ def get_access_token_with_code(code: str):
                                  "code": code,
                                  "client_id": client_id,
                                  "client_secret": client_secret,
-                                 "redirect_uri": "http://127.0.0.1:5000/auth",
+                                 "redirect_uri": redirect_uri,
                                  "grant_type": "authorization_code"
                              })
 
     data = response.json()
     access_token = data.get('access_token')
     refresh_token = data.get('refresh_token')
+
+    if not access_token or not refresh_token:
+        raise Exception(f'{response.content}')
 
     return {"access_token": access_token, "refresh_token": refresh_token}
 
@@ -30,11 +35,19 @@ def refresh_access_token():
     refresh_token = request.cookies.get('refresh_token')
 
     token_url = current_app.config.get('TOKEN_URL')
+    client_id = current_app.config.get('CLIENT_ID')
+    client_secret = current_app.config.get('CLIENT_SECRET')
+    authorization = base64.b64encode(
+        f'{client_id}:{client_secret}'.encode('utf-8')
+    ).decode('utf-8')
 
     response = requests.post(token_url,
+                             headers={
+                                 "Authorization": f"Basic {authorization}"
+                             },
                              data={
                                  "grant_type": "refresh_token",
-                                 "client_id": "edv",
+                                 "client_id": client_id,
                                  "refresh_token": refresh_token
                              })
     
@@ -43,13 +56,16 @@ def refresh_access_token():
     return data.get('access_token')
 
 
-def get_userinfo():
+def get_userinfo(token:str=None):
     userinfo_url = current_app.config.get('USERINFO_URL')
     access_token = request.cookies.get('access_token')
 
     response = requests.get(userinfo_url, 
                             headers={
-                                "Authorization": f"Bearer {access_token}"
+                                "Authorization": f"Bearer {token if token else access_token}"
                             })
+    
+    if response.status_code == 401:
+        raise Exception('Unauthorized!')
     
     return response.json()
